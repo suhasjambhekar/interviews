4df6df981f48db1f599e68a669c4af8f8b182471
suhasjambhekar <30352888+suhasjambhekar@users.noreply.github.com>
2017-07-21 16:36:03 -0400

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -5,6 +5,11 @@ import java.util.Set;
 /**
 
 TODO: Make this class immutable.
+Steps:
+1) remove field Setters (commented here for past reference) 
+2) provide a constructor for setting 
+3) remove default constructor
+4) make class final
 
 */
 public class Exercise1 {
@@ -15,28 +20,33 @@ public class Exercise1 {
 		private String firstName;
 		private String lastName;
 		
-		public Person() {
+		//public Person() {//Remove default constructor
+		//}
+		public Person(String firstName,String lastName,Set<String> phoneNumbers) {
+			this.firstName = firstName;	
+			this.lastName = lastName;	
+			this.phoneNumbers = phoneNumbers;	
 		}
 
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
+		//public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		//	phoneNumbers = newPhoneNumbers;
+		//}
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
+		//public void setFirstName(String newName) {
+		//	firstName = newName;
+		//}
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		//public void setLastName(String newName) {
+		//	lastName = newName;
+		//}
 	}
 }
